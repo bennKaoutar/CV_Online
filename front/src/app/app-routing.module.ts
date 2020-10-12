bde2ffdecdd242ff44b6325f70fc132ee228de10
Cecile V <56889291+CVhp@users.noreply.github.com>
2020-10-12 14:59:59 +0200

    front base form template

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ListUsersComponent} from './pages/list-users/list-users.component';
-import {AddUserComponent} from './pages/add-user/add-user.component';
+
+import { ListUsersComponent } from './pages/list-users/list-users.component';
+import { AddUserComponent } from './pages/add-user/add-user.component';
+import { CvTemplateComponent } from './pages/cv-template/cv-template.component';
 
 
 const routes: Routes = [
   { path: '', component: ListUsersComponent },
   { path: 'list', component: ListUsersComponent },
-  { path: 'add-user', component: AddUserComponent }];
+  { path: 'add-user', component: AddUserComponent },
+  { path: 'cv-template', component: CvTemplateComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { CvTemplateComponent } from './pages/cv-template/cv-template.component';
 import { ListUsersComponent } from './pages/list-users/list-users.component';
 import { AddUserComponent } from './pages/add-user/add-user.component';
-import { HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
-import { CvTemplateComponent } from './pages/cv-template/cv-template.component';
 
 @NgModule({
   declarations: [
