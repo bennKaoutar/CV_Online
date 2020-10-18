555a906183f1e58dc830380d0009ef173a3354c7
Cecile V <56889291+CVhp@users.noreply.github.com>
2020-10-18 18:48:40 +0200

    try add mat tab

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -25,7 +26,8 @@ import { CvViewComponent } from './pages/cv-view/cv-view.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
